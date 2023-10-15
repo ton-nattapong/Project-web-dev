e4bdc22765132222f0b9ae6634801af1846e66bc
ton-nattapong <s6404062636323@email.kmutnb.ac.th>
2023-10-15 20:41:10 +0700

    s

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
 include "connect.php";
 ?>
-
 <html>
     <head>
         <meta charset="utf-8">
@@ -12,8 +11,7 @@ include "connect.php";
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
         <script>
-            
-            var idnum = {}; // ต้องใช้ var หรือ let ในการประกาศตัวแปร
+            var idnum = {};
             var count = 0; 
             function addToCart(menuNo) {
                 var xhr = new XMLHttpRequest();
@@ -26,6 +24,7 @@ include "connect.php";
                 var idnumStr = idnum.join(',');
                 window.location.href = "cart.php?menu_No=" + idnum;
             }
+            
             function changetheme() {
                 var content = document.getElementById("content");
                 var topnav = document.getElementById("topnav");
@@ -34,24 +33,23 @@ include "connect.php";
                 topnav.style.background = "#27374D";
                 serchbox.style.background = "#526D82";
             }
-           
-            </script>
-            
+      
+        </script>
             
     </head>
 <body>
     
-    <header>
-        <div class="topnav" id="topnav">     
-            <a href="#"><img src="./image/logo.png" height="30px" ></a>
+    <div class="wrapper">
+        <div class="topnav" id="topnav">   
+            <a href="setting.php"><img src="./image/logo.png" height="30px" ></a>
             <a class = "linkbutt" style="color:#FBB813;" href="frist.php">Shop</a>
-            <a class = "linkbutt" href="showquery.php">Query</a>
             <a class = "linkbutt" href="logout.php">logout</a>
-            <a class = "cartbutt" href="cart.php"><i class="fa-solid fa-cart-shopping fa-xl"></i></a>             
-            <button class = "button-color" onclick="changetheme()">dark mode  <i class="fa-solid fa-moon fa-xl" style="color: #000000;"></i></button>
+            <a class = "linkbutt" href="showquery.php">Query</a>
+            <a class = "cartbutt" href="cart.php"><i class="fa-solid fa-cart-shopping fa-xl"></i></a>
+            <button class = "button-color" onclick="changetheme()">Dark</button>
              
         </div>
-             <form  class="serch-box" id="serch-box">
+            <form  class="serch-box" id="serch-box">
                 <br><input type="text" id="serch-name" name="serch-name" placeholder="  ค้นหาเมนู">
                 
                  
@@ -59,9 +57,8 @@ include "connect.php";
                     <i class="fa-solid fa-magnifying-glass fa-xl"></i> 
                 </button>
          
-        </form>
-            
-        <main id="content">
+            </form>
+        <div class="content" id="content">
           
             <?php
                 $itemsPerPage = 10; // กำหนดจำนวนรายการที่จะแสดงในแต่ละหน้า
@@ -78,38 +75,31 @@ include "connect.php";
                 </p>
                 <p id="pricetag">
                     <?= $row["price"] . " บาท   " ?>
-                </p><br>
+                </p>
                 <div class="container">
                     <i id="center-button" class="fa-solid fa-cart-plus fa-2xl" name="bt-cart" onclick="addToCart(<?php echo $row["menu_No"]; ?>)"></i> <br><br>
                 </div>
             </div>
-            <?php endwhile; ?>
-            </div>
-           
-        
-        </main>
-        <div class="pagination">
-                <?php if ($page > 1): ?>
-                <a href="?page=<?= ($page - 1) ?>">หน้าก่อนหน้า</a>
-
-                <?php endif; ?>
-
-                <?php
-                $stmt->closeCursor(); // ปิดการค้นหาคิวรี่ก่อนหน้า
-                $stmt = $pdo->query("SELECT COUNT(*) FROM menu");
-                $totalItems = $stmt->fetchColumn();
-                $totalPages = ceil($totalItems / $itemsPerPage);
-
-                if ($page < $totalPages): ?>
-
-                <a href="?page=<?= ($page + 1) ?>">หน้าถัดไป</a>
-
-                <?php endif; ?>
-            </div>
-        
-        <div class="footer">
-            
+        <?php endwhile; ?>
         </div>
-   </header>
+        <div class="pagination">
+            <?php if ($page > 1): ?>
+            <a href="?page=<?= ($page - 1) ?>">หน้าก่อนหน้า</a>
+
+            <?php endif; ?>
+
+            <?php
+            $stmt->closeCursor(); // ปิดการค้นหาคิวรี่ก่อนหน้า
+            $stmt = $pdo->query("SELECT COUNT(*) FROM menu");
+            $totalItems = $stmt->fetchColumn();
+            $totalPages = ceil($totalItems / $itemsPerPage);
+
+            if ($page < $totalPages): ?>
+
+            <a href="?page=<?= ($page + 1) ?>">หน้าถัดไป</a>
+
+            <?php endif; ?>
+         </div>
+    </div>
 </body>
 </html>
